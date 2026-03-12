cc2ec2dece3e6d2cdb449ae840df620fb155acbd
Ansiee3103 <Ansiee3103@gmail.com>
2026-03-12 15:23:25 +0530

    test ci cd

--- services/auth-service/index.js
+++ services/auth-service/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Auth Service Running");
+    res.send("Auth Service Running v2");
 });
 
 app.listen(3000, () => {
